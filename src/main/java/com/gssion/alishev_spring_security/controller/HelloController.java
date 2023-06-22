083aef1cf544fd7cf6bd8f3ed89ddc2005badb69
Soslan Gassiev <soslangassity04@gmail.com>
2023-06-22 23:02:17 +0300

    feat : add showUser

--- src/main/java/com/gssion/alishev_spring_security/controller/HelloController.java
+++ src/main/java/com/gssion/alishev_spring_security/controller/HelloController.java
@@ -1,8 +1,10 @@
 package com.gssion.alishev_spring_security.controller;
 
+import com.gssion.alishev_spring_security.security.PersonDetails;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HelloController {
@@ -10,5 +12,15 @@ public class HelloController {
     @GetMapping("/hello")
     public String hello() {
         return "hello";
+    }
+
+    @GetMapping("/showUserInfo")
+    public String showUserInfo() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        PersonDetails personDetails = (PersonDetails) authentication.getPrincipal();
+        System.out.println(personDetails.getPerson());
+
+        return "hello";
+
     }
 }
